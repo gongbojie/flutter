b2a6c8c80daea47f54107d31b870b924a7690aa8
GonG <gong0906@foxmail.com>
2022-01-11 17:57:59 +0800

    Composing Card2: the author card

--- Fooderlich/lib/home.dart
+++ Fooderlich/lib/home.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'card1.dart';
+import 'card2.dart';
 
 // 1
 class Home extends StatefulWidget {
@@ -15,10 +17,8 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
   // 8
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with Card1
-    Container(color: Colors.red),
-    // TODO: Replace with Card2
-    Container(color: Colors.green),
+    const Card1(),
+    const Card2(),
     // TODO: Replace with Card3
     Container(color: Colors.blue),
   ];
